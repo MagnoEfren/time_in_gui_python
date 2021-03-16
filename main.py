e26150946ff91009af6e6eee5437eeae68a1d160
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-03-16 00:59:18 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import  QTimer, QTime
+
+from PyQt5.uic import loadUi
+import time
+
+
+class VentanaPrincipal(QMainWindow):
+
+    def __init__(self):
+        super(VentanaPrincipal, self).__init__()
+        loadUi('GUI4.ui', self)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.displayTime)
+        timer.start(1000)
+
+
+    def displayTime(self):
+        currentTime = QTime.currentTime()
+        displayText = currentTime.toString('hh:mm:ss')
+        self.reloj.setText(displayText)
+        self.lcdNumber.display(displayText)
+  
+
+
+app = QApplication(sys.argv)
+main = VentanaPrincipal()
+main.show()
+sys.exit(app.exec_())
