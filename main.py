2bb9e478ef750baa633a022d871fbc002745a2ea
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-12-13 19:40:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import  QTimer, QTime
 from PyQt5.uic import loadUi
 
-
-
 class VentanaPrincipal(QMainWindow):
-
     def __init__(self):
         super(VentanaPrincipal, self).__init__()
         loadUi('GUI4.ui', self)
@@ -15,15 +12,12 @@ class VentanaPrincipal(QMainWindow):
         timer.timeout.connect(self.displayTime)
         timer.start(1000)
 
-
     def displayTime(self):
         currentTime = QTime.currentTime()
         displayText = currentTime.toString('hh:mm:ss')
         self.reloj.setText(displayText)
         self.lcdNumber.display(displayText)
   
-
-
 app = QApplication(sys.argv)
 main = VentanaPrincipal()
 main.show()
